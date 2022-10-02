734882493273d51d96d8a019138043aa059679f6
GeriVoodCoder <riazwaan03@gmail.com>
2022-10-02 21:58:54 +0100

    Solution to primes

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    prime = 2
+    while 1:
+        if len(list) < number_of_primes:
+            for i in range(2,prime):
+                if prime % i == 0:
+                    prime += 1
+                    break
+            else:
+                list.append(prime)
+                prime += 1
+        else:
+            break
     return list
